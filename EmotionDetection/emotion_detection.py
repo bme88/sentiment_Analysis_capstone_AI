8796819c16023e5eaf7cd3f865a9f23439e4a88a
bme88 <voquangha281@gmail.com>
2025-04-09 09:34:14 -0400

    Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,6 +3,10 @@ def emotion_detector(text_to_analyse):
     """
         This function helps to detect emotion from text
     """
+    if text_to_analyse=="":
+        return {'anger' : None, 'disgust' : None, 'fear' : None, 'joy' : None, 
+                'sadness' : None, 'dominant_emotion' : None}
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
--- server.py
+++ server.py
@@ -4,23 +4,16 @@ import json
 
 app = Flask("Emotion Detection")
 
-@app.route("/")
-def render_index_page():
-    return render_template('index.html')
-
 @app.route("/emotionDetector")
 def emotionDetector():
     text = request.args.get('textToAnalyze')
     result = ed.emotion_detector(text)
-
     dominant_emotion=result.pop("dominant_emotion",None)
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
 
     string = ', '.join([f"'{key}' : {value}" for key, value in result.items()])
-
-    return_string = f"For the given statement, the system response is {string}. The dominant emotion is {dominant_emotion}." 
-
-
-    return return_string
-
+    re_string = f"For the given statement, the system response is {string}. The dominant emotion is {dominant_emotion}." 
+    return re_string
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)

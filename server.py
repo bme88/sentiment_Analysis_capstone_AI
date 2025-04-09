b40a4d9dcbef27997228f15415de4950a3dcbbdb
bme88 <voquangha281@gmail.com>
2025-04-09 09:55:59 -0400

    Starndarding coding style with Pylint

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request
+"""
+    This server hosts API for sentiment detector
+"""
+from flask import Flask, request
 from EmotionDetection import emotion_detection as ed
-import json
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detector():
+    """
+     This function receive textToAnalyze from the interface and send
+     back the sentimental analysis
+    """
     text = request.args.get('textToAnalyze')
     result = ed.emotion_detector(text)
     dominant_emotion=result.pop("dominant_emotion",None)
@@ -13,7 +19,10 @@ def emotionDetector():
         return "Invalid text! Please try again!"
 
     string = ', '.join([f"'{key}' : {value}" for key, value in result.items()])
-    re_string = f"For the given statement, the system response is {string}. The dominant emotion is {dominant_emotion}." 
+    re_string = f"For the given statement, the system response \
+    is {string}.The dominant emotion is {dominant_emotion}."
     return re_string
+
 if __name__ == "__main__":
+
     app.run(host='127.0.0.1', port=5000)
